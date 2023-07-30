05ebbc1c872231f8709efd743fcf1031a31743c1
Charles Borges <solucao.erp@gmail.com>
2023-07-30 13:03:16 -0300

    SumApp: implementando a funcionalidade em 'sum.component...'

--- apps/sum-app/src/app/sum/sum/sum.component.ts
+++ apps/sum-app/src/app/sum/sum/sum.component.ts
@@ -7,10 +7,26 @@ import { SumService } from '../services';
   styleUrls: ['./sum.component.css']
 })
 export class SumComponent implements OnInit {
+  private resultado: number = 0;
 
   constructor(private sumService: SumService) { }
 
   ngOnInit(): void {
+  }
+
+  // Argumentos em string, porque vem da tela 'form'
+  somar(numero1: string, numero2: string): void {
+    let n1: number;
+    let n2: number;
+
+    n1 = parseFloat(numero1);
+    n2 = parseFloat(numero2);
+    this.resultado = this.sumService.somar(n1, n2);
+  }
+
+  // Mostra, devolve o resultado guardado na variavel 'resultado' e processada no método 'somar()'
+  get resultSomaShow(): string {
+    return this.resultado.toString();
   }
 
 }
