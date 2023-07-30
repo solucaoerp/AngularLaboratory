53a0f154394d2eb3f42b03ba1a02376f3dba5b20
Charles Borges <solucao.erp@gmail.com>
2023-07-30 11:37:07 -0300

    SumApp: 'barrel file' para componentes, boas praticas

--- apps/sum-app/src/app/sum/sum.module.ts
+++ apps/sum-app/src/app/sum/sum.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SumComponent } from './sum/sum.component';
+import { SumComponent } from './sum';
 
 @NgModule({
   declarations: [
