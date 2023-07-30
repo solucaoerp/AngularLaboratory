266e3c61be98d7c44ae3f7c5b7dc513cb63b5c44
Charles Borges <solucao.erp@gmail.com>
2023-07-30 17:59:12 -0300

    SumApp: implements service e component 'subtraction'

--- apps/sum-app/src/app/subtraction/services/subtraction.service.ts
+++ apps/sum-app/src/app/subtraction/services/subtraction.service.ts
@@ -6,4 +6,11 @@ import { Injectable } from '@angular/core';
 export class SubtractionService {
 
   constructor() { }
+
+  subtracao(numero1: number, numero2: number): number {
+    let resultado: number;
+
+    resultado = numero1 - numero2;
+    return resultado;
+  }
 }
