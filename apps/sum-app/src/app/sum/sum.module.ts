3968b7ad61910bc6424b28e98ae647c00ea6b2d7
Charles Borges <solucao.erp@gmail.com>
2023-07-30 11:31:03 -0300

    SumApp: Criacao de componente 'sum.component...', estrutura padrao

--- apps/sum-app/src/app/sum/sum.module.ts
+++ apps/sum-app/src/app/sum/sum.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SumComponent } from './sum/sum.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SumComponent
+  ],
   imports: [
     CommonModule
   ]
---
+++ apps/sum-app/src/app/sum/sum/sum.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sum',
+  templateUrl: './sum.component.html',
+  styleUrls: ['./sum.component.css']
+})
+export class SumComponent {
+
+}
