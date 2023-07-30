29a92af6315288b316dc57455efae0e02ca60ab1
Charles Borges <solucao.erp@gmail.com>
2023-07-30 12:01:02 -0300

    SumApp: criacao de servico 'sum.service.ts', regras de negocio

---
+++ apps/sum-app/src/app/sum/services/sum.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SumService {
+
+  constructor() { }
+
+  somar(numero1: number, numero2: number): number {
+    let resultado: number;
+    resultado = numero1 + numero2;
+    return resultado;
+  }
+}
