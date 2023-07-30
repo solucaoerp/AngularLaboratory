c88b4b25199d79806bf15d7c52d9a0e4792ad665
Charles Borges <solucao.erp@gmail.com>
2023-07-30 12:14:23 -0300

    SumApp: usando o service (injection) em 'sum.component'

--- apps/sum-app/src/app/sum/sum/sum.component.ts
+++ apps/sum-app/src/app/sum/sum/sum.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SumService } from '../services';
 
 @Component({
   selector: 'app-sum',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SumComponent implements OnInit {
 
-  constructor() { }
+  constructor(private sumService: SumService) { }
 
   ngOnInit(): void {
   }
