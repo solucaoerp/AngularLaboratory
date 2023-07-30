73d8316157e9dd9fa7748324fb8669690e221192
Charles Borges <solucao.erp@gmail.com>
2023-07-30 11:43:38 -0300

    SumApp: estruturando o componente 'sum.component.ts', boas praticas, ciclo de vida

--- apps/sum-app/src/app/sum/sum/sum.component.ts
+++ apps/sum-app/src/app/sum/sum/sum.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sum',
   templateUrl: './sum.component.html',
   styleUrls: ['./sum.component.css']
 })
-export class SumComponent {
+export class SumComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 
 }
