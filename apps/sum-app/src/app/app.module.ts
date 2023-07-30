21349cc37d3331c0fc64b067cfc2defe42d2dda3
Charles Borges <solucao.erp@gmail.com>
2023-07-30 11:18:46 -0300

    Modulo 'sum' criado e configurado em app.module.ts (raiz)

--- apps/sum-app/src/app/app.module.ts
+++ apps/sum-app/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SumModule } from './sum/sum.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SumModule
   ],
   providers: [],
   bootstrap: [AppComponent]
