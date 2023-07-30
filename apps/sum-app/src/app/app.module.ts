d4abacc5e96571ac969ae9ebb3078362138491f7
Charles Borges <solucao.erp@gmail.com>
2023-07-30 17:15:33 -0300

    SumApp: modulo 'subtraction' criado

--- apps/sum-app/src/app/app.module.ts
+++ apps/sum-app/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SumModule } from './sum';
+import { SubtractionModule } from './subtraction';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { SumModule } from './sum';
   ],
   imports: [
     BrowserModule,
-    SumModule
+    SumModule,
+    SubtractionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
