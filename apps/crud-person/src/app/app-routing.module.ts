907ad5d86655dc5b0a57496eb30740e173b74847
Charles Borges <solucao.erp@gmail.com>
2023-07-31 23:17:52 -0300

    crud-person: rota 'persons/list' em 'AppRoutingModule' configurada

--- apps/crud-person/src/app/app-routing.module.ts
+++ apps/crud-person/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PersonListComponent } from './components/person/person-list/person-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'persons/list', pathMatch: 'full' },
+  { path: 'persons', redirectTo: 'persons/list' },
+  { path: 'persons/list', component: PersonListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
