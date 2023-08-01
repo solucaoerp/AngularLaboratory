a47c2a522dcb87f3ec6ab9e92c2d602751c8bc3f
Charles Borges <solucao.erp@gmail.com>
2023-07-31 22:26:12 -0300

    crud-person: configurando 'PersonService' em 'Providers'

--- apps/crud-person/src/app/modules/person/person.module.ts
+++ apps/crud-person/src/app/modules/person/person.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PersonService } from 'src/app/services/person/person.service';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule]
+  imports: [CommonModule],
+  providers: [PersonService]
 })
 export class PersonModule { }
