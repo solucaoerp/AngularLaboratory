cf6be7a29120a144605854b640e33aeb37476668
Charles Borges <solucao.erp@gmail.com>
2023-07-31 21:47:04 -0300

    crud-person: modulo 'person' para gerenciar os dados + config

--- apps/crud-person/src/app/app.module.ts
+++ apps/crud-person/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { PersonModule } from './person/person.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PersonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
