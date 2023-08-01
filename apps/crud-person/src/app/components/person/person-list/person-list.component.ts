7cc2addc104fbfde9d35f353a0a0c38cb50e1e1c
Charles Borges <solucao.erp@gmail.com>
2023-07-31 23:11:05 -0300

    crud-person: component 'PersonListComponent' criado e configurado 'AppModule'

---
+++ apps/crud-person/src/app/components/person/person-list/person-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-person-list',
+  templateUrl: './person-list.component.html',
+  styleUrls: ['./person-list.component.css']
+})
+export class PersonListComponent {
+
+}
